1b198cd6a74906264e1f7b59a4ee408bd26cded6
Mochammad Rizki Romdoni <cv.yacanet@gmail.com>
2024-01-03 22:28:51 +0700

    configuration files

---
+++ FrmLogin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using static System.Windows.Forms.AxHost;
+
+using System.Configuration;
+
+namespace simrs
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string dbHost = ConfigurationManager.AppSettings.Get("db_host");
+            MessageBox.Show(dbHost);
+        }
+    }
+}
