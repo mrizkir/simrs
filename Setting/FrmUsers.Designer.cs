87efc646eadc40c5988a51169c62a1f7ad12fb10
Mochammad Rizki Romdoni <cv.yacanet@gmail.com>
2024-01-07 18:51:48 +0700

    tambah combobox default role

--- Setting/FrmUsers.Designer.cs
+++ Setting/FrmUsers.Designer.cs
@@ -29,16 +29,41 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbRoles = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.cmbRoles);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 214);
             this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DEFAULT ROLE";
+            // 
+            // cmbRoles
+            // 
+            this.cmbRoles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRoles.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbRoles.FormattingEnabled = true;
+            this.cmbRoles.Location = new System.Drawing.Point(175, 31);
+            this.cmbRoles.Name = "cmbRoles";
+            this.cmbRoles.Size = new System.Drawing.Size(254, 32);
+            this.cmbRoles.TabIndex = 1;
             // 
             // FrmUsers
             // 
@@ -52,6 +77,9 @@
             this.Name = "FrmUsers";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "USERS";
+            this.Load += new System.EventHandler(this.FrmUsers_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -59,5 +87,7 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbRoles;
     }
 }
--- Setting/FrmUsers.cs
+++ Setting/FrmUsers.cs
@@ -16,5 +16,26 @@ namespace simrs.Setting
         {
             InitializeComponent();
         }
+
+        void setDaftarRoles()
+        {
+            Dictionary<int, string> daftarRoles = new Dictionary<int, string>();
+
+            daftarRoles.Add(-1, "- PILIH ROLE -");
+            daftarRoles.Add(1, "SUPERADMIN");
+            daftarRoles.Add(2, "DOKTER");
+            daftarRoles.Add(3, "PASIEN");
+
+            this.cmbRoles.DataSource = new BindingSource(daftarRoles, null);
+            this.cmbRoles.DisplayMember = "Value";
+            this.cmbRoles.ValueMember = "Key";
+
+        }
+
+
+        private void FrmUsers_Load(object sender, EventArgs e)
+        {
+            this.setDaftarRoles();
+        }
     }
 }
